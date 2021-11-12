0626ebec6d8fe18f67c4bafe69e6d57772f89670
hyunjae cho <chohyunjae163@gmail.com>
2021-11-12 22:47:46 +0900

    completed functionality of checkers

--- src/game.rs
+++ src/game.rs
@@ -72,6 +72,19 @@ impl GameEngine {
         })
     }
 
+    pub fn get_piece(&self, coord : Coordinate) -> Result<Option<GamePiece>,()> {
+        let Coordinate(x,y) = coord;
+        if x <= 7 && y <= 7 {
+            Ok(self.board[x][y])
+        } else {
+            Err(())
+        }
+    }
+
+    pub fn current_turn(&self) -> PieceColor {
+        self.current_turn
+    }
+
     fn should_crown(&self, piece: GamePiece, coord: Coordinate) -> bool {
         let Coordinate(_x, y) = coord;
 
